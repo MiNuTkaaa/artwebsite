ddcff4f4d5eaf73a47fded8586b4ef273ff3492b
MiNuTkaaa <72147926+MiNuTkaaa@users.noreply.github.com>
2025-05-17 23:48:32 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Initial display
+showSlide(currentSlide);
+setInterval(nextSlide, 10000); // change every 10 seconds
